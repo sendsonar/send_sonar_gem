a4f93102616cbcfdb5192d5942e1daa1038e44e6
Elijah Murray <elijah@bluefantail.com>
2014-06-19 13:26:38 -0700

    initial tests working, basic api calls working

--- lib/simple_desk.rb
+++ lib/simple_desk.rb
@@ -1,5 +1,54 @@
 require "simple_desk/version"
+require "net/http"
+require "uri"
 
-module SimpleDesk
-  # Your code goes here...
+class Api
+	BASE_URL = "https://www.getsimpledesk.com"
+	TOKEN = ENV['SIMPLE_DESK_TOKEN'] || "ucMKQtZ0CQkgfGzTj6lOJe2VRvoRHM8z"
+
+	def add_customer(params)
+		uri = URI.parse(build_url("add_customer", params))
+		response = Net::HTTP.get_response(uri)
+	end	
+	alias_method :update_customer, :add_customer
+
+
+	def build_url(type = nil, params=nil)
+		url = post_url(type)
+
+		if params
+			string = ''
+			url << "&"
+			params.each { |k,v| string << "#{k}=#{v}&" }
+
+			url << string
+			url = url[0...-1] if (url[-1] == '&')
+		end
+		return url
+	end
+
+	def post_url(type)
+		if type == nil
+			"#{BASE_URL}?token=#{TOKEN}"
+		else
+			case type
+			when "message_customer"
+				"#{BASE_URL}/api_send_message?token=#{TOKEN}"
+			when "add_customer" || "update_customer"
+				"#{BASE_URL}/api_add_customer?token=#{TOKEN}"
+			end
+		end
+	end
+
+	def message_customer(message_and_phone_number)
+		url = post_url
+		url << "&" 
+		string = ''
+		message_and_phone_number.each { |k,v| string << "#{k}=#{v}&" }
+		url << string
+		url = post_url[0...-1] if post_url[-1]
+	end
+
+		# https://www.getsimpledesk.com/api_send_message
+
 end
